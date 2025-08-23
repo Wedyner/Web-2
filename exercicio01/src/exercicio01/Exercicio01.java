41f554446eebff3ef1464e5dfed2c84e71007713
Wedyner R. Maciel <124943637+Wedyner@users.noreply.github.com>
2025-08-22 22:27:32 -0300

    Update Exercicio01.java

--- exercicio01/src/exercicio01/Exercicio01.java
+++ exercicio01/src/exercicio01/Exercicio01.java
@@ -2,17 +2,15 @@ package exercicio01;
 
 
 public class Exercicio01 {
-    // Atributos privados para garantir o encapsulamento
+    
     private String nome;
     private int idade;
     
-    // Construtor que recebe nome e idade (obrigatórios)
     public Exercicio01(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
     
-    // Métodos getters para acessar os dados
     public String getNome() {
         return nome;
     }
@@ -21,7 +19,6 @@ public class Exercicio01 {
         return idade;
     }
     
-    // Métodos setters para modificar os dados
     public void setNome(String nome) {
         this.nome = nome;
     }
