94dee6e41d2beb3f3925f5a9515e5abd4e7f0d96
Wedyner R. Maciel <124943637+Wedyner@users.noreply.github.com>
2025-08-22 22:28:15 -0300

    Update principal.java

--- exercicio01/src/exercicio01/principal.java
+++ exercicio01/src/exercicio01/principal.java
@@ -3,12 +3,11 @@ package exercicio01;
 public class principal {
 
     public static void main(String[] args) {
-        // a. Instanciar uma pessoa com valores predefinidos
+       
         Exercicio01 pessoa = new Exercicio01("João Silva", 25);
         
-        // b. Modificar o nome e a idade com novos valores predefinidos
-        pessoa.setNome("Maria Oliveira");  // Modifica o nome
-        pessoa.setIdade(30);               // Modifica a idade
+        pessoa.setNome("Maria Oliveira");  
+        pessoa.setIdade(30);               
               
         System.out.println("Nova idade: " + pessoa.getIdade());
         
