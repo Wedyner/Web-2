aea30b5db1ea377764b4a3ae14f763bbdafe0e94
Wedyner R. Maciel <124943637+Wedyner@users.noreply.github.com>
2025-08-24 18:39:11 -0300

    Update Exercicio03.java

--- Exercicio03/src/Exercicio03.java
+++ Exercicio03/src/Exercicio03.java
@@ -1,17 +1,14 @@
 
 public class Exercicio03 {
 
-    // Atributos privados para garantir o encapsulamento
     private int codigo;
     private String titulo;
     
-    // Construtor que recebe código e título (obrigatórios)
     public Exercicio03(int codigo, String titulo) {
         this.codigo = codigo;
         this.titulo = titulo;
     }
     
-    // Métodos getters para acessar os dados
     public int getCodigo() {
         return codigo;
     }
@@ -20,7 +17,6 @@ public class Exercicio03 {
         return titulo;
     }
     
-    // Métodos setters para modificar os dados
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
@@ -29,7 +25,6 @@ public class Exercicio03 {
         this.titulo = titulo;
     }
     
-    // Método para representação em string
     @Override
     public String toString() {
         return "Código: " + codigo + " - Título: " + titulo;
